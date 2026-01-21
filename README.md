5a63fad538cdbc6e27f97d1bfd4f3ba8fe392cbc
Traube <ben.trautner@web.de>
2026-01-21 21:59:04 +0100

    feat: add README.md

---
+++ README.md
@@ -0,0 +1,61 @@
+# vexo-fabric
+
+![release](https://img.shields.io/github/v/release/MagischeTraube/vexo-fabric?color=blue&label=Release)
+![downloads](https://img.shields.io/github/downloads/MagischeTraube/vexo-fabric/total?color=blue&label=Downloads)
+![license](https://img.shields.io/github/license/MagischeTraube/vexo-fabric?color=blue&label=License)
+[![discord](https://img.shields.io/discord/1385629270352859296?color=blue&label=Discord)](https://discord.gg/wfW3aEEpVA)
+
+
+Hypixel Skyblock 1.21.10 Mod focussing on Dungeons and Quality of Life.
+
+> [!NOTE]
+> If you own a Github account, **please consider leaving a Star** and forking the Repo! Doing this will help us get more feedback to provide you more and better Features.
+
+## Features
+
+<details>
+<summary>Dungeons</summary>
+
+- Leap Alerts
+- Rag Axe Alerts
+- Pad Timer
+- Particle Hider
+- Party Finder
+- Positional Mesaages
+</details>
+
+<details>
+<summary>Kuudra</summary>
+
+- Auto Kuudra requeue
+</details>
+
+<details>
+<summary>QOL</summary>
+
+- SlayerHelper
+- Chat Cleaner
+- Auto Rejoin
+</details>
+
+<details>
+<summary>Commands</summary>
+
+* /vexo - Opens the mod configuration menu.
+* /tyfr - Will leave the Party after the current the Dungeon/Kuudra run
+* /&lt;floor or kuudra tier&gt;
+    * /f5 -> Floor 5
+    * /m7 -> Master Mode 7
+    * /t4 -> Fiery Kuudra
+</details>
+
+**And more to come!**
+> [!NOTE]
+> This mod is under active development, and we’d love to hear your feedback! Please join our Discord to submit feature requests or report any bugs.
+
+---
+
+# Dependencies
+
+* **Fabric API**: [Download it from Modrinth](https://modrinth.com/mod/fabric-api)
+* **Fabric Language Kotlin**: [Download it from Modrinth](https://modrinth.com/mod/fabric-language-kotlin)
--- src/main/kotlin/xyz/vexo/features/impl/dungeons/ParticleHider.kt
+++ src/main/kotlin/xyz/vexo/features/impl/dungeons/ParticleHider.kt
@@ -5,6 +5,7 @@ import xyz.vexo.config.impl.BooleanSetting
 import xyz.vexo.events.EventHandler
 import xyz.vexo.events.impl.ChatMessagePacketEvent
 import xyz.vexo.events.impl.ParticleSpawnEvent
+import xyz.vexo.events.impl.WorldJoinEvent
 import xyz.vexo.features.Module
 import xyz.vexo.utils.removeFormatting
 
@@ -12,8 +13,8 @@ object ParticleHider : Module (
     name = "Particle Hider",
     description = "Hides various particles"
 ) {
-    private val p5Hider by BooleanSetting("hide in p5")
-    private val p4Hider by BooleanSetting("hide in p4")
+    private val p5Hider by BooleanSetting("Hide in p5")
+    private val p4Hider by BooleanSetting("Hide in p4")
     private val iceSpray by BooleanSetting("Hide Ice Spray")
 
     var inP5 = false
@@ -21,16 +22,24 @@ object ParticleHider : Module (
 
     @EventHandler
     fun onChat(event: ChatMessagePacketEvent) {
-        val cleanMessage = event.message.removeFormatting()
-        if (cleanMessage == "[BOSS] Necron: Let's make some space!") inP5 = true
-        if (cleanMessage matches Regex("\\[BOSS] Necron: You went further than any human before, congratulations\\.")) inP4 = true
+        if (event.unformattedMessage == "[BOSS] Necron: Let's make some space!") inP5 = true
+        if (event.unformattedMessage matches Regex("\\[BOSS] Necron: You went further than any human before, congratulations\\.")) inP4 = true
     }
 
     @EventHandler
     fun onParticle(event: ParticleSpawnEvent){
-        if (inP5 && p5Hider) { event.cancel() }
+        if (inP5 && p5Hider) { event.cancel()
+            inP4 = false
+        }
         if (inP4 && p4Hider) { event.cancel() }
         if (event.particle is ExplodeParticle && iceSpray) event.cancel()
 
     }
+
+    @EventHandler()
+    fun onWorldJoin (event: WorldJoinEvent){
+        inP5 = false
+        inP4 = false
+    }
+
 }
